239a91d49e1599011bfca09d1cca701d26dc5cb8
한강학 <kanghakhan@tmon.co.kr>
2017-06-28 20:20:29 +0900

    Apply Spring Initializer

---
+++ src/test/java/com/kanghack2/example/springEx/SpringExApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kanghack2.example.springEx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringExApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
